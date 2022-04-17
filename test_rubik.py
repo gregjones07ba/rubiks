b3f1c47df39f90863af74f2bc7e8ea7edec41948
Greg Jones <grjones@zulily.com>
2022-04-17 15:58:48 -0700

    Consolidate tests.

--- test_rubik.py
+++ test_rubik.py
@@ -10,31 +10,28 @@ class TestRubik(unittest.TestCase):
     
     def test_rotate_rgb(self):
         gb = self.rubik.get_cell(0, 0, 2)
-        gb.walls[0].description = "foo"
+        gb.walls[0].description = "gb"
         rb = self.rubik.get_cell(2, 0, 0)
-        rb.walls[0].description = "bar"
+        rb.walls[0].description = "rb"
         rg = self.rubik.get_cell(0, 2, 0)
-        rg.walls[0].description = "baz"
+        rg.walls[0].description = "rg"
         gby = self.rubik.get_cell(-2, -2, 2)
-        gby.walls[0].description = "Boo!"
+        gby.walls[0].description = "gby"
+        center = self.rubik.get_cell(0, 0, 0)
+        center.walls[0].description = "center"
         
         self.rubik.rotate(0)
 
         gb_again = self.rubik.get_cell(2, 0, 0)
-        self.assertEqual(gb_again.walls[0].description, "foo")
+        self.assertEqual(gb_again.walls[0].description, "gb")
         rb_again = self.rubik.get_cell(0, 2, 0)
-        self.assertEqual(rb_again.walls[0].description, "bar")
+        self.assertEqual(rb_again.walls[0].description, "rb")
         rg_again = self.rubik.get_cell(0, 0, 2)
-        self.assertEqual(rg_again.walls[0].description, "baz")
+        self.assertEqual(rg_again.walls[0].description, "rg")
         gby_again = self.rubik.get_cell(-2, -2, 2)
-        self.assertEqual(gby_again.walls[0].description, "Boo!")
-
-    def test_rotate_leaves_center(self):
-        center = self.rubik.get_cell(0, 0, 0)
-        center.walls[0].description = "Boo!"
-        self.rubik.rotate(0)
+        self.assertEqual(gby_again.walls[0].description, "gby")
         center_again = self.rubik.get_cell(0, 0, 0)
-        self.assertEqual(center_again.walls[0].description, "Boo!")
+        self.assertEqual(center_again.walls[0].description, "center")
 
     def test_rotate_rotates_octa(self):
         rgb_octa = self.rubik.get_cell(1, 1, 1)
