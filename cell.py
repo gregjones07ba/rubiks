308d15abe4bcd5da82b05137efc9830a05fb6ea2
Greg Jones <grjones@zulily.com>
2022-04-17 16:41:11 -0700

    Make walls private.

--- cell.py
+++ cell.py
@@ -6,13 +6,20 @@ from color_directions import tetra_colors, octa_colors
 class Cell:
     def tetra_factory(x, y, z):
         c = Cell(x, y, z)
-        c.walls = [Wall(color, direction) for color, direction in tetra_colors]
+        c.__walls = [Wall(color, direction) for color, direction in tetra_colors]
         return c
 
     def octa_factory(x, y, z):
         c = Cell(x, y, z)
-        c.walls = [Wall(color, direction) for color, direction in octa_colors]
+        c.__walls = [Wall(color, direction) for color, direction in octa_colors]
         return c
 
     def __init__(self, x, y, z):
         self.coords = array((x, y, z))
+        self.rotation = array(
+            ((1, 0, 0),
+             (0, 1, 0),
+             (0, 0, 1))) # identity
+
+    def get_walls(self):
+        return self.__walls
--- test_rubik.py
+++ test_rubik.py
@@ -9,67 +9,67 @@ class TestRubik(unittest.TestCase):
     
     def test_rotate_rgb(self):
         gb = self.rubik.get_cell(0, 0, 2)
-        gb.walls[0].description = "gb"
+        gb.get_walls()[0].description = "gb"
         rb = self.rubik.get_cell(2, 0, 0)
-        rb.walls[0].description = "rb"
+        rb.get_walls()[0].description = "rb"
         rg = self.rubik.get_cell(0, 2, 0)
-        rg.walls[0].description = "rg"
+        rg.get_walls()[0].description = "rg"
         gby = self.rubik.get_cell(-2, -2, 2)
-        gby.walls[0].description = "gby"
+        gby.get_walls()[0].description = "gby"
         center = self.rubik.get_cell(0, 0, 0)
-        center.walls[0].description = "center"
+        center.get_walls()[0].description = "center"
         rgb_octa = self.rubik.get_cell(1, 1, 1)
-        rgb_octa.walls[0].description = "rgb_octa"
+        rgb_octa.get_walls()[0].description = "rgb_octa"
         rgb = self.rubik.get_cell(2, 2, 2)
-        rgb.walls[0].description = "rgb"
+        rgb.get_walls()[0].description = "rgb"
         gby_octa = self.rubik.get_cell(-1, -1, 1)
-        gby_octa.walls[0].description = "gby_octa"
+        gby_octa.get_walls()[0].description = "gby_octa"
         
         self.rubik.rotate(0)
 
         gb_again = self.rubik.get_cell(2, 0, 0)
-        self.assertEqual(gb_again.walls[0].description, "gb")
+        self.assertEqual(gb_again.get_walls()[0].description, "gb")
         rb_again = self.rubik.get_cell(0, 2, 0)
-        self.assertEqual(rb_again.walls[0].description, "rb")
+        self.assertEqual(rb_again.get_walls()[0].description, "rb")
         rg_again = self.rubik.get_cell(0, 0, 2)
-        self.assertEqual(rg_again.walls[0].description, "rg")
+        self.assertEqual(rg_again.get_walls()[0].description, "rg")
         gby_again = self.rubik.get_cell(-2, -2, 2)
-        self.assertEqual(gby_again.walls[0].description, "gby")
+        self.assertEqual(gby_again.get_walls()[0].description, "gby")
         center_again = self.rubik.get_cell(0, 0, 0)
-        self.assertEqual(center_again.walls[0].description, "center")
+        self.assertEqual(center_again.get_walls()[0].description, "center")
         rgb_octa_again = self.rubik.get_cell(1, 1, 1)
-        self.assertEqual(rgb_octa_again.walls[0].description, "rgb_octa")
+        self.assertEqual(rgb_octa_again.get_walls()[0].description, "rgb_octa")
         rgb_again = self.rubik.get_cell(2, 2, 2)
-        self.assertEqual(rgb_again.walls[0].description, "rgb")
+        self.assertEqual(rgb_again.get_walls()[0].description, "rgb")
         gby_octa_again = self.rubik.get_cell(-1, -1, 1)
-        self.assertEqual(gby_octa_again.walls[0].description, "gby_octa")
+        self.assertEqual(gby_octa_again.get_walls()[0].description, "gby_octa")
 
     def test_rotate_rgy(self):
         rg = self.rubik.get_cell(0, 2, 0)
-        rg.walls[0].description = "rg"
+        rg.get_walls()[0].description = "rg"
 
         self.rubik.rotate(1)
 
         rg_again = self.rubik.get_cell(0, 0, -2)
-        self.assertEqual(rg_again.walls[0].description, "rg")
+        self.assertEqual(rg_again.get_walls()[0].description, "rg")
 
     def test_rotate_gby(self):
         gb = self.rubik.get_cell(0, 0, 2)
-        gb.walls[0].description = "gb"
+        gb.get_walls()[0].description = "gb"
 
         self.rubik.rotate(2)
 
         gb_again = self.rubik.get_cell(-2, 0, 0)
-        self.assertEqual(gb_again.walls[0].description, "gb")
+        self.assertEqual(gb_again.get_walls()[0].description, "gb")
 
     def test_rotate_rby(self):
         rb = self.rubik.get_cell(2, 0, 0)
-        rb.walls[0].description = "rb"
+        rb.get_walls()[0].description = "rb"
 
         self.rubik.rotate(3)
 
         rb_again = self.rubik.get_cell(0, -2, 0)
-        self.assertEqual(rb_again.walls[0].description, "rb")
+        self.assertEqual(rb_again.get_walls()[0].description, "rb")
 
 if __name__ == '__main__':
     unittest.main()
