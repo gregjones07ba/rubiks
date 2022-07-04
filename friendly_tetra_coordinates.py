708cc7937ec3e1b551c85e4ce902d88ebf2b7219
Greg Jones <grjones@zulily.com>
2022-07-03 21:02:55 -0700

    Scale correctly for vertical.

--- friendly_tetra_coordinates.py
+++ friendly_tetra_coordinates.py
@@ -4,9 +4,9 @@ from numpy import array
 
 class FriendlyTetraCoordinates(FriendlyCoordinates):
     def __init__(self,
-                 vertical = array((1.0/3.0, 1.0/3.0, 1.0/3.0)),
+                 vertical = array((1.0/4.0, 1.0/4.0, 1.0/4.0)),
                  vertical_offset = 1.0,
-                 depth = array((-1.0/3.0, -1.0/3.0, 1.0/3.0)),
+                 depth = array((-1.0/4.0, -1.0/4.0, 1.0/4.0)),
                  depth_offset = 1.0,
                  horizontal = array((-1.0/2.0, 1.0/2.0, 0.0)),
                  horizontal_offset = 0.0):
--- test_friendly_tetra_coordinates.py
+++ test_friendly_tetra_coordinates.py
@@ -14,5 +14,11 @@ class TestFriendlyTetraCoordinates(unittest.TestCase):
         expected_coords = (2, 0, 0)
         self.assertEquals(actual_coords, expected_coords)
 
+    def test_friendly_coords_returns_0_0_2_for_rgb(self):
+        location = array((2, 2, 2))
+        actual_coords = self.test_obj.friendly_coords(location)
+        expected_coords = (0, 0, 2)
+        self.assertEquals(actual_coords, expected_coords)
+
 if __name__ == '__main__':
     unittest.main()
